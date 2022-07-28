9b20877746b3f7fc29fd031b68a8f3279b2c224a
Sunny Lan <sunny.lan@uwaterloo.ca>
2022-07-28 16:51:24 -0400

    ok

--- downbad/Program.cs
+++ downbad/Program.cs
@@ -20,12 +20,20 @@ namespace downbad
         [STAThread]
         static void Main()
         {
+
             Mat filter(Mat img)
             {
                 return img.InRange(new Scalar(250, 250, 250), new Scalar(256, 256, 256));
             }
+
+            Mat filterCheckmark(Mat img)
+            {
+                return img.InRange(new Scalar(145, 219, 36), new Scalar(149,223,40));
+            }
+
             var speechbubble=filter(Cv2.ImRead("speechbubble.png", ImreadModes.Color));//.ConvertTo(speechbubble, MatType.CV_64FC1,1/255.0f);
-            var checkmark=Cv2.ImRead("checkmark.png", ImreadModes.Color);//.ConvertTo(speechbubble, MatType.CV_64FC1,1/255.0f);
+            var chatplaying = filter(Cv2.ImRead("chatplaying.png", ImreadModes.Color));//.ConvertTo(speechbubble, MatType.CV_64FC1,1/255.0f);
+            var checkmark=filterCheckmark(Cv2.ImRead("checkmark.png", ImreadModes.Color));//.ConvertTo(speechbubble, MatType.CV_64FC1,1/255.0f);
             var result = new Mat();
             var dpiScale = 1.25;
             while (true)
@@ -35,10 +43,10 @@ namespace downbad
 
                 var screenshot = ImageFinder.MakeScreenshot();
                 var screen = (screenshot.ToMat().CvtColor(ColorConversionCodes.BGRA2BGR));//.ConvertTo(screen, MatType.CV_64FC1,1/255.0f);
+                Mat filteredScreen = filter(screen);
 
                 Console.WriteLine("Looking for speech bubble");
-                Cv2.MatchTemplate(filter(screen), speechbubble, result, TemplateMatchModes.SqDiffNormed);
-                Cv2.ImShow("speech", result);
+                Cv2.MatchTemplate(filteredScreen, speechbubble, result, TemplateMatchModes.SqDiffNormed);
                 result.MinMaxLoc(out var score, out var _, out var offset, out var _);
                 Console.WriteLine($" Found pos = {offset} score = {score}");
 
@@ -50,7 +58,7 @@ namespace downbad
                 else
                 {
                     Console.WriteLine("Looking for checkmark");
-                    Cv2.MatchTemplate(screen, checkmark, result, TemplateMatchModes.SqDiffNormed);
+                    Cv2.MatchTemplate(filterCheckmark( screen), checkmark, result, TemplateMatchModes.SqDiffNormed);
                     result.MinMaxLoc(out score, out var _, out offset, out var _);
                     Console.WriteLine($" Found pos = {offset} score = {score}");
 
@@ -61,13 +69,23 @@ namespace downbad
                     }
                     else
                     {
-                        Console.WriteLine("Nothing found, press space");
-                        AutoItX.Send(" ");
+
+                        Console.WriteLine("Checking if in chat mode");
+                        Mat corner = filter(screen.SubMat(0, chatplaying.Height, 0, chatplaying.Width));
+                        Cv2.MatchTemplate(corner, chatplaying, result, TemplateMatchModes.SqDiffNormed);
+                        result.MinMaxLoc(out  score, out var _, out var _, out var _);
+                        Console.WriteLine($" Score = {score}");
+                        if (score < 0.5)
+                        {
+                            Console.WriteLine("Nothing found, press space");
+                            AutoItX.Send(" ");
+                        }
                     }
                 }
                 Cv2.WaitKey(500);
 
                 screen.Dispose();
+                screenshot.Dispose();
             }
 
 
